0a3e39395dab637ab8d87c653ee775c4586fd304
lishuai.jing <lishuai.jing@velux.com>
2022-04-20 13:58:56 +0200

    minor changes

--- apps/app_st.py
+++ apps/app_st.py
@@ -1,18 +1,13 @@
 
 
 import streamlit as st
-from utils.pkg1 import add_int # You can train models, data processing etc.
+from utils.pkg1 import add_int  # You can train models, data processing etc.
 
 input1 = st.sidebar.number_input(
     'Input 1', min_value=0, max_value=100, value=25)
 
 input2 = st.sidebar.number_input(
     'Input 2', min_value=0, max_value=100, value=25)
-
-tax_schema = st.sidebar.selectbox(
-    'Tax schema', ['Business', 'Personal'])
-
-
 
 st.title('A simple calculation tool')
 
@@ -26,7 +21,4 @@ st.markdown(html_temp, unsafe_allow_html=True)
 # Create a button, that when clicked, shows a text
 if(st.button("Calculate")):
 
-    st.write(add_int(input1,input2))
-	
-   
-    
+    st.write(add_int(input1, input2))
